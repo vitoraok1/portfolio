29de781b7d71d578c56dfe81ed0ddf56bbdfd8cf
Vitor Aoki <vitoraoki17@gmail.com>
2023-06-30 12:20:10 -0300

    skills section html, css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]') 
 
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('filters__active')
+    })
+    target.classList.add('filters__active')
+
+    tabs.forEach(tab => {
+      tab.classList.remove('filter-tab-active')
+    })
+
+    tab.classList.add('filter-tab-active')
+  })
+})
 
 /*=============== DARK LIGHT THEME ===============*/
 
